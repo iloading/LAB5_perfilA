b456f4a84a0876e9c1690450f68122cdc9025f1c
Samuel Santos <48221347+iloading@users.noreply.github.com>
2020-11-24 21:27:32 +0000

    aula 13 added

---
+++ aula13/src/Database.js
@@ -0,0 +1,36 @@
+class Database {
+  constructor() {
+    this.data = [
+      {
+        id: 1,
+        nome: "Maria Antonia",
+        username: "mant",
+        gn: "Feminino",
+        tlf: "3519394459",
+      },
+      {
+        id: 2,
+        nome: "Josefa Maria",
+        username: "josm",
+        gn: "Feminino",
+        tlf: "3519394458",
+      },
+      {
+        id: 3,
+        nome: "Mario Joao",
+        username: "mj",
+        gn: "Masculino",
+        tlf: "3519394456",
+      },
+      {
+        id: 4,
+        nome: "Samuel Santos",
+        username: "ss",
+        gn: "Masculino",
+        tlf: "999999999",
+      },
+    ];
+  }
+}
+
+export default Database;
